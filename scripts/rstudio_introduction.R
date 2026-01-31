83732979a1c3a44b2d2c9e7dfc642d45ba91680a
Tendai Pasipamire <sinyoro.pams1982@gmail.com>
2026-01-31 13:23:52 +0200

    scripts cleanup

--- scripts/rstudio_introduction.R
+++
@@ -1,8 +0,0 @@
-install.packages("highcharter")
-library(highcharter)
-attach(women)
-highcharter::hchart(height)
-ebola_data <- read.csv("https://tinyurl.com/ebola-data-sample")
-ebola_data
-?hchart
-?women
